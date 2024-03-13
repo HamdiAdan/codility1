19accaecc33928bb8cb1772ff9edff5ade512bf4
Hamdi Adan <hhhaji013@gmail.com>
2024-03-14 01:02:57 +0300

    completed

--- transaction.js
+++ transaction.js
@@ -1,17 +1,22 @@
 function solution(A, D, N) {
+    //initialisation
+    
     let balance = 0;
     let monthlyFee = 0;
     let cardPayments = 0;
     let currentMonth = new Date('2020-01-01');
 
     for (let i = 0; i < N; i++) {
+         // Convert transaction date 
         let transactionDate = new Date(D[i]);
 
+    // Checking  if the transaction date is in a different month than the current month
         if (transactionDate.getFullYear() > currentMonth.getFullYear() ||
             transactionDate.getMonth() > currentMonth.getMonth()) {
             currentMonth.setMonth(currentMonth.getMonth() + 1);
             cardPayments = 0;
-            if (currentMonth.getMonth() !== 0) { // Apply monthly fee for all months except January
+            if (currentMonth.getMonth() !== 0) {
+        // Apply monthly fee for all months except January
                 monthlyFee += 5;
             }
         }
@@ -28,7 +33,8 @@ function solution(A, D, N) {
         }
     }
 
-    balance -= monthlyFee * 12; // Deduct total monthly fee for the year
+    balance -= monthlyFee * 12; 
+    // Deduct total monthly fee for the year
 
     return balance;
 }
